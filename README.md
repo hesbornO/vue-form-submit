2c94c5f4ab0bd1aad4c279e91fa34897e45f9b27
hesbornO <oswago54@gmail.com>
2021-01-11 22:52:03 +0300

    simple vue form submit using axios and json test server

---
+++ README.md
@@ -0,0 +1,33 @@
+# vue-form-submit
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### YouTube tutorial
+[Post API with form data](https://www.youtube.com/watch?v=XWfxsDcoqe4)
+
+### JSON test server
+https://github.com/typicode/json-server
+
+### VUE AXIOS for form data posting
+https://www.npmjs.com/package/vue-axios
---
+++ src/components/PostComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <form @submit="postData" method="post">
+          <label for=""></label> <input type="text" placeholder="author" name="author" v-model="posts.author"> <br> <br>
+          <label for=""></label> <input type="text" placeholder="title" name="title" v-model="posts.title"> <br> <br>
+          <button type="submit">Post</button>
+      </form>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+export default {
+    name: "PostComponent",
+    data(){
+        return {
+            posts:{
+                title:null,
+                author:null
+            }
+        }
+
+    },
+    methods:{
+        postData(e){
+            // console.warn(this.posts)
+            this.axios.post("http://localhost:3000/posts",this.posts)
+            .then((result)=>{
+                console.warn(result)
+            })
+            e.preventDefault();
+                        
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
