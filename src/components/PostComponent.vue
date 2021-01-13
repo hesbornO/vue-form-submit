880de0a5aebd25241cfcad946fbde3913a466557
hesbornO <oswago54@gmail.com>
2021-01-13 22:24:57 +0300

    nesting form data and posting to jsonServer

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
       <form @submit="postData" method="post">
-          <label for=""></label> <input type="text" placeholder="author" name="author" v-model="posts.author"> <br> <br>
-          <label for=""></label> <input type="text" placeholder="title" name="title" v-model="posts.title"> <br> <br>
+          <label for="author">Author: </label> 
+          <input type="text" placeholder="author" name="author" v-model="posts.author"> <br> <br>
+          
+          <label for="title">Title: </label> 
+          <input type="text" placeholder="title" name="title" v-model="posts.title"> <br> <br>
+          
+          <label for="production">Production year: </label> 
+          <input type="text" placeholder="production" name="production_year" v-model="posts.release_data.production_year"> <br> <br>          
+          
+          <label for="publication-year">Publication year: </label> 
+          <input type="text" placeholder="1998" name="publication_year" v-model="posts.release_data.publication.publication_year"> <br> <br>          
+          
+          <label for="production">Publication month : </label> 
+          <input type="text" placeholder="" name="publication_month" v-model="posts.release_data.publication.publication_month"> <br> <br>          
+          
           <button type="submit">Post</button>
       </form>
   </div>
@@ -18,10 +31,17 @@ Vue.use(VueAxios, axios)
 export default {
     name: "PostComponent",
     data(){
-        return {
+        return {             
             posts:{
                 title:null,
-                author:null
+                author:null,
+                release_data:{
+                    "production_year": null,
+                    publication : {
+                        publication_year: null,
+                        publication_month: null
+                    }
+                }
             }
         }
 
